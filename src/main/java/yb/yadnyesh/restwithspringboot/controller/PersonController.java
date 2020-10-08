9b663e9dbc3d597a33efb2598c1c9bad0dba302a
yadnyesh <yadnyesh.juvekar@gmail.com>
2020-10-08 10:00:14 +0530

    Update and Delete without MySQL

--- src/main/java/yb/yadnyesh/restwithspringboot/controller/PersonController.java
+++ src/main/java/yb/yadnyesh/restwithspringboot/controller/PersonController.java
@@ -34,4 +34,17 @@ public class PersonController {
         return personService.createPerson(person);
     }
 
+    @RequestMapping(method=RequestMethod.PUT,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person update(@RequestBody Person person) {
+        return personService.update(person);
+    }
+
+    @RequestMapping(value="/{id}",
+            method=RequestMethod.DELETE)
+    public void delete(@PathVariable("id") String id) {
+        personService.delete(id);
+    }
+
 }
